c1f673ef82a6af0c314523e0ac9f08e5d239a1e8
Paritosh <kpari307@gmail.com>
2021-01-01 22:34:32 +0530

    Added post router and added succesfully to the file

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+app.use(express.json());
+
 app.get('/api/v1/tours', (req, res) => {
   res.json({
     status: 'success',
@@ -14,37 +16,37 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id * 1;
-  const foundTour = tours.find((element) => element.id === id);
+// app.get('/api/v1/tours/:id', (req, res) => {
+//   const id = req.params.id * 1;
+//   const foundTour = tours.find((element) => element.id === id);
 
-  if (id > tours.length) {
-    return res.status(400).json({
-      message: 'Invalid ID lol',
-      status: ' LOL ',
-    });
-  }
+//   if (id > tours.length) {
+//     return res.status(400).json({
+//       message: 'Invalid ID lol',
+//       status: ' LOL ',
+//     });
+//   }
 
-  res.send('omkay you got it');
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-  res.send('Got your request the given function will be implemented');
-});
+//   res.send('omkay you got it');
+// });
 
 //we need a middleware in case of a post request a middleware is basically a function which is implemented between a request response cycle
 
-app.post('api/v1/tours', (req, res) => {
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, req.body);
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length -1].id  + 1;
+  const newTour = Object.assign({id : newID} , req.body);
 
   tours.push(newTour);
-  fs.writeFile();
-  res.send(
-    'The request has been sent to server to be added in database , Thank you for your cooperation'
-  );
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json` , JSON.stringify(tours) , (err)=>{
+      res.status(400).json({
+           status : "Successfully added",
+           data: {
+               tours : newTour
+           }
+      })
+  })
+  
 });
-
 const port = 3000;
 app.listen(port, () => {
   console.log('Server had started');
